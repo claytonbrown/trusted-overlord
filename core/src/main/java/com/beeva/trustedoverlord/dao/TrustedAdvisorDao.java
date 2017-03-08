5ee0c3cd5391bb22344c15bcee288033759a557a
gonzaloramos <gonzalo.ramos@beeva.com>
2017-03-08 15:26:55 +0100

    Small fix

--- core/src/main/java/com/beeva/trustedoverlord/dao/TrustedAdvisorDao.java
+++ core/src/main/java/com/beeva/trustedoverlord/dao/TrustedAdvisorDao.java
@@ -1,6 +1,5 @@
 package com.beeva.trustedoverlord.dao;
 
-import com.beeva.trustedoverlord.clients.SupportClient;
 import com.beeva.trustedoverlord.model.ProfileChecks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +8,7 @@ import org.apache.logging.log4j.Logger;
  * Created by gonzaloramos on 13/02/17.
  */
 public interface TrustedAdvisorDao {
-    static Logger logger = LogManager.getLogger(SupportClient.class);
+    static Logger logger = LogManager.getLogger(TrustedAdvisorDao.class);
 
     boolean saveData(String profileName, ProfileChecks profileChecks) throws Exception;
 
