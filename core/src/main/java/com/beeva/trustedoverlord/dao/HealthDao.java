f291ebb20ab9db4821b9601416a67c816ba2b859
gonzaloramos <gonzalo.ramos@beeva.com>
2017-03-10 10:05:25 +0100

    Async Health DAO

--- core/src/main/java/com/beeva/trustedoverlord/dao/HealthDao.java
+++ core/src/main/java/com/beeva/trustedoverlord/dao/HealthDao.java
@@ -1,14 +1,15 @@
 package com.beeva.trustedoverlord.dao;
 
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 import com.beeva.trustedoverlord.model.ProfileHealth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.CompletableFuture;
 
 
 public interface HealthDao {
     static Logger logger = LogManager.getLogger(HealthDao.class);
 
-    boolean saveData(String profileName, ProfileHealth profileHealth) throws Exception;
-
-    ProfileHealth getData(String profile) throws Exception;
+    public void saveDataAsync(String profileName, ProfileHealth profileHealth, final CompletableFuture<PutItemResult> future) throws Exception;
 }
--- core/src/main/java/com/beeva/trustedoverlord/dao/HealthDynamoDBDao.java
+++ core/src/main/java/com/beeva/trustedoverlord/dao/HealthDynamoDBDao.java
@@ -1,55 +1,72 @@
 package com.beeva.trustedoverlord.dao;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.handlers.AsyncHandler;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 import com.beeva.trustedoverlord.model.ProfileHealth;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 
 public class HealthDynamoDBDao implements HealthDao {
-    public boolean saveData(String profileName, ProfileHealth profileHealth) {
-        AmazonDynamoDBAsync client = AmazonDynamoDBAsyncClientBuilder.standard().withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration(System.getenv("AWS_DYNAMODB_ENDPOINT"),
-                        System.getenv("AWS_DYNAMODB_REGION")))
+    private AmazonDynamoDBAsync dynamoClient;
+
+    public HealthDynamoDBDao() {
+        this.dynamoClient = AmazonDynamoDBAsyncClientBuilder
+                .standard()
+                .withCredentials(new ProfileCredentialsProvider("default"))
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                System.getenv("AWS_DYNAMODB_ENDPOINT"),
+                                System.getenv("AWS_DYNAMODB_REGION")
+                        )
+                )
                 .build();
+    }
 
-        DynamoDB dynamoDB = new DynamoDB(client);
-
-        Table table = dynamoDB.getTable("overlord");
-
+    public void saveDataAsync(String profileName, ProfileHealth profileHealth, final CompletableFuture<PutItemResult> future) {
         String type = "health" + "-" + profileName;
-
 
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String date = now.format(formatter);
 
-        final Map<String, Object> infoMap = new HashMap<String, Object>();
-        infoMap.put("openIssues",  profileHealth.getOpenIssues());
-        infoMap.put("scheduledChanges",  profileHealth.getScheduledChanges());
-        infoMap.put("otherNotifications",  profileHealth.getOtherNotifications());
+        final Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+        item.put("otype", new AttributeValue(type));
+        item.put("odate", new AttributeValue(date));
+        item.put("openIssues",  new AttributeValue().withSS(profileHealth.getOpenIssues()));
+        item.put("scheduledChanges",  new AttributeValue().withSS(profileHealth.getScheduledChanges()));
+        item.put("otherNotifications",  new AttributeValue().withSS(profileHealth.getOtherNotifications()));
 
-        try {
-            table.putItem(new Item()
-                    .withPrimaryKey("otype", type, "odate", date)
-                    .withMap("info", infoMap));
-            logger.info("PutItem succeeded: " + type + " " + date);
-        } catch (Exception e) {
-            logger.error("Unable to add item: " + type + " " + date);
-        }
+        Future<PutItemResult> putItemResult = this.dynamoClient.putItemAsync(
+                new PutItemRequest()
+                        .withTableName("overlord")
+                        .withItem(item),
+                new AsyncHandler<PutItemRequest, PutItemResult>() {
+                    @Override
+                    public void onError(Exception exception) {
+                        logger.error("Unable to add item: " + type + " " + date);
+                        future.completeExceptionally(exception);
+                    }
 
-        return true;
+                    @Override
+                    public void onSuccess(PutItemRequest request, PutItemResult putItemRes) {
+                        logger.info("PutItem succeeded: " + type + " " + date);
+                        future.complete(putItemRes);
+                    }
+                }
+        );
     }
-
-    public ProfileHealth getData(String profile) {
-        return null;
-    }
-
 }
